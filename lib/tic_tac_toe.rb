4c84de9de79f71335dcfe991cf4fbefc5cb23d11
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-09-21 18:46:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,3 +46,20 @@ def winner(board)
     board[winner.first]
   end
 end
+def current_player(board)
+  if turn_count(board).even?
+  "X"
+else
+  "O"
+end
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |player|
+    if player == "X" || player == "O"
+      turns += 1
+    end
+  end
+  turns
+end
